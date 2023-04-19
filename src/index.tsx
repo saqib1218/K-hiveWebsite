c75abdb34167b6481a9977bb8a10a6c42ad3312f
dawar_latif <dawarlatif30@gmail.com>
2023-04-19 10:17:16 +0500

    logo changed base url set in .env

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
